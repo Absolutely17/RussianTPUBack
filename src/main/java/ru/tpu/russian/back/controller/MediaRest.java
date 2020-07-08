d64a5c7fa40b6dd06b175cc401033ad8e3ad7489
Ilya Suhkorukov <SuhorukovIO@sib-soft.ru>
2020-07-08 14:02:01 +0700

    Тестирование получения изображений. Временное решение исключительно для теста.

---
+++ src/main/java/ru/tpu/russian/back/controller/MediaRest.java
@@ -0,0 +1,30 @@
+package ru.tpu.russian.back.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.tpu.russian.back.dto.ImageResponseDto;
+import ru.tpu.russian.back.service.MediaService;
+
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(value = "/media", produces = APPLICATION_JSON_UTF8_VALUE)
+public class MediaRest {
+
+    private MediaService mediaService;
+
+    public MediaRest(MediaService mediaService) {
+        this.mediaService = mediaService;
+    }
+
+    @RequestMapping(method = GET, path = "/img/{id}")
+    public String getImage(
+            @PathVariable String id
+    ) throws IOException {
+        return mediaService.getImage(id);
+    }
+
+}
---
+++ src/main/java/ru/tpu/russian/back/service/MediaService.java
@@ -0,0 +1,29 @@
+package ru.tpu.russian.back.service;
+
+import org.springframework.stereotype.Service;
+import ru.tpu.russian.back.dto.ImageResponseDto;
+
+import javax.imageio.ImageIO;
+import java.awt.image.*;
+import java.io.*;
+import java.util.Base64;
+
+@Service
+public class MediaService {
+
+//    private MediaRepository mediaRepository;
+//
+//    public MediaService(MediaRepository mediaRepository) {
+//        this.mediaRepository = mediaRepository;
+//    }
+
+    public String getImage(String id) throws IOException {
+        File img = new File("C:/test.jpg");
+        BufferedImage bufferedImage = ImageIO.read(img);
+        WritableRaster raster = bufferedImage.getRaster();
+        DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+
+        return Base64.getEncoder().encodeToString(data.getData());
+    }
+
+}

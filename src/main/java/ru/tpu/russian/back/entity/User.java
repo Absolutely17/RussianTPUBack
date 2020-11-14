23fc354bc0ed7d79d5fad2f44bdf616e61e7d9ce
Ilya Suhkorukov <suhorukovio@ssp-soft.com>
2020-11-14 14:26:15 +0700

    Поменял хранение в БД у пользователя ID группы на название группы

--- src/main/java/ru/tpu/russian/back/entity/User.java
+++ src/main/java/ru/tpu/russian/back/entity/User.java
@@ -60,8 +60,8 @@ public class User {
     @Column(name = "CONFIRMED")
     private boolean isConfirm;
 
-    @Column(name = "GROUP_ID")
     @Nullable
+    @Column(name = "GROUP_NAME")
     private String groupName;
 
     public User(
